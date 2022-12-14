8ef75e479777f43ab67ebc186fa4e1178a67e707
Saikat2203 <ssarkar1997@gmail.com>
2022-12-14 16:56:01 +0530

    committing

--- PatientRegistrationSystem/src/main/java/com/nissan/service/PatientServiceImpl.java
+++ PatientRegistrationSystem/src/main/java/com/nissan/service/PatientServiceImpl.java
@@ -2,6 +2,8 @@ package com.nissan.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import com.nissan.dao.PatientDao;
 import com.nissan.entity.Patient;
 
 @Service
+@Transactional
 public class PatientServiceImpl implements IPatientService {
 
 	@Autowired
